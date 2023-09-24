b0c20ba63910f0d5522c077cd1bd089076ff0afe
localgaji <oorongcha@naver.com>
2023-09-25 06:08:16 +0900

    feat: 로그인 회원가입 관련 페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React, { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import KakaoAuth from 'auth/KakaoAuth';
+import LoginPage from 'pages/LoginPage';
+import SigninPage from 'pages/SigninPage';
+
 function App(): JSX.Element {
   return (
     <BrowserRouter>
@@ -10,6 +14,9 @@ function App(): JSX.Element {
         <Suspense fallback={<p>로딩...</p>}>
           <Routes>
             <Route path={'/'} element={<div>메인...</div>} />
+            <Route path={'/login'} element={<LoginPage />} />
+            <Route path={'/login/kakao'} element={<KakaoAuth />} />
+            <Route path={'/signin'} element={<SigninPage />} />
           </Routes>
         </Suspense>
       </ErrorBoundary>
