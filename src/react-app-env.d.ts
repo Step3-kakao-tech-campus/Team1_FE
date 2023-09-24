ab17861da5f81d73e4b369114ed6427bb21b4a67
localgaji <oorongcha@naver.com>
2023-09-25 03:20:58 +0900

    feat: 로그인 - 환경변수 redirect_uri 타입 지정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,7 +3,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     REACT_APP_API_URL: string;
     REACT_APP_BASE_URL: string;
-    REACT_APP_IMAGE: string;
     REACT_APP_KAKAO_API_KEY: string;
+    REACT_APP_KAKAO_REDIRECT_URI: string;
   }
 }
