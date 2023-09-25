2df14a9facab9e5ac4b96c769279b098f17181a7
localgaji <oorongcha@naver.com>
2023-09-25 18:03:42 +0900

    fix: 로그인 - 토큰 발행 url 범위 확장

--- src/apis/instance.tsx
+++ src/apis/instance.tsx
@@ -26,8 +26,9 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   (response) => {
+    console.log(response);
     // 다른 데이터는 body에 담는데 토큰은 header에 담으므로 로그인 요청일땐 따로 지정
-    if (response.config.url === '/auth/login') {
+    if (response.config.url?.includes('/auth')) {
       return response.headers.authorization;
     }
     return response.data.response;
