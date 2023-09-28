640f0295e5dd576af579aef5d8ae7dafc32827b6
harin9 <83578728+harin9@users.noreply.github.com>
2023-09-29 03:03:36 +0900

    feat: NextButton 생성

---
+++ src/components/atoms/NextButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
+
+type Props = {
+  onClick: () => void;
+  size: string;
+  color: string;
+};
+
+const NextButton = ({ onClick, size, color }: Props) => {
+  return (
+    <button onClick={() => onClick}>
+      <IoIosArrowForward size={size} color={color} />
+    </button>
+  );
+};
+
+export default NextButton;
