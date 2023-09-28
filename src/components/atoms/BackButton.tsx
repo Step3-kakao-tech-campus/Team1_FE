91353eab207925fec478f865e295f7ab59612e0d
harin9 <83578728+harin9@users.noreply.github.com>
2023-09-29 02:58:26 +0900

    feat: BackButton 생성

---
+++ src/components/atoms/BackButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+
+type Props = {
+  onClick: () => void;
+  size: string;
+  color: string;
+};
+
+const BackButton = ({ onClick, size, color }: Props) => {
+  return (
+    <button onClick={() => onClick}>
+      <IoMdArrowRoundBack size={size} color={color} />
+    </button>
+  );
+};
+
+export default BackButton;
