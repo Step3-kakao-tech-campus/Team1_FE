4ea3632c1d9d80f5d60f9fc52e5e86e5b45eaa73
localgaji <oorongcha@naver.com>
2023-09-29 14:19:20 +0900

    feat: 회원가입 페이지 추가 (기능만 구현)

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,35 +1,41 @@
-import { postSignin } from 'apis/join';
+import { postSignin } from 'apis/signin';
+import useForm from 'hooks/useForm';
 import useLogin from 'hooks/useLogin';
 import React, { useState } from 'react';
+import signinValidator from 'utils/signinValidator';
 
 const SigninPage = (): JSX.Element => {
+  // 1. 알바/매니저 선택
+  type adminType = boolean | null;
+
+  const [isAdmin, setIsAdmin] = useState<adminType>(null);
+  const selectTypeHandler = (isAdminParam: adminType): void => {
+    setIsAdmin((prev: adminType) => isAdminParam);
+  };
+
+  // 2. 폼 입력 상태 관리
   interface StateType {
     [index: string]: string | boolean;
     name: string;
     agreement: boolean;
   }
 
-  const [userInfo, setUserInfo] = useState<StateType>({
+  const initalInfo: StateType = {
     name: '',
     agreement: false,
-  });
-
-  const formHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newObj = { ...userInfo, [event.target.id]: event.target.value };
-    setUserInfo((prev: StateType) => newObj);
   };
 
-  const checkboxHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newObj = { ...userInfo, [event.target.id]: !userInfo[event.target.id] };
-    setUserInfo((prev: StateType) => newObj);
-  };
+  const { obj: userInfo, formHandler, toggleHandler } = useForm(initalInfo);
 
-  const { login } = useLogin();
+  // 3. 로그인 요청 관리
+  const { saveLogin } = useLogin();
 
   const SigninHandler = () => {
-    postSignin(userInfo.name)
+    if (isAdmin === null) return;
+    postSignin(userInfo.name, isAdmin)
       .then((response) => {
-        login('토큰');
+        // 토큰 저장
+        saveLogin(response);
       })
       .catch((error) => {
         // 에러 처리
@@ -38,17 +44,31 @@ const SigninPage = (): JSX.Element => {
 
   return (
     <div>
-      <div>
-        <label htmlFor="name">이름을 입력하세요</label>
-        <input id="name" onChange={formHandler} />
-      </div>
-      <div>
-        <input id="agreement" onChange={checkboxHandler} type="checkbox" />
-        <label htmlFor="name">약관동의</label>
-      </div>
-      <button onClick={SigninHandler} disabled={userInfo.name.length < 2 || userInfo.agreement != true}>
-        가입 완료
-      </button>
+      {isAdmin === null ? (
+        <>
+          <button id="admin" onClick={() => selectTypeHandler(true)}>
+            매니저
+          </button>
+          <button id="employee" onClick={() => selectTypeHandler(false)}>
+            알바생
+          </button>
+        </>
+      ) : (
+        <div>
+          <div>
+            <label htmlFor="name">이름을 입력하세요</label>
+            <input id="name" onChange={formHandler} />
+          </div>
+          <div>
+            <input id="agreement" onChange={toggleHandler} type="checkbox" />
+            <label htmlFor="name">약관동의</label>
+          </div>
+
+          <button onClick={SigninHandler} disabled={!signinValidator(userInfo)}>
+            가입 완료
+          </button>
+        </div>
+      )}
     </div>
   );
 };
