14734067bf981239b290bfa156597d0089d194d8
localgaji <oorongcha@naver.com>
2023-10-01 04:34:07 +0900

    feat: 초대링크 페이지에 로그인 컴포넌트 추가

--- src/pages/InvitedPage.tsx
+++ src/pages/InvitedPage.tsx
@@ -1,18 +1,17 @@
 import React, { Suspense, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from 'states/store';
 import { getGroupInvitation, postGroupJoin } from 'apis/groupInvite';
 import { useQuery } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
-import { convertPath } from 'apis/convertURI';
+import LoginOrSignup from 'components/organisms/LoginOrSignup';
 
 interface Component {}
 
 const InvitedPage = ({}: Component): JSX.Element => {
   const param = useParams()?.invitationKey;
   const invitationKey: string = !!param ? param : '';
-  const navigate = useNavigate();
 
   const loginState = useSelector((state: RootState) => state.login);
   const [isModal, setIsModal] = useState(false);
@@ -23,21 +22,15 @@ const InvitedPage = ({}: Component): JSX.Element => {
     switch (loginState.islogin) {
       case false: // 비로그인 상태일 때
         setIsModal((prev) => true); // 로그인/회원가입 모달
-        // 로컬에 초대키 킵하기
-        // 카카오 redirect uri 고민해보기
-        // login/kakao 페이지를 팝업처리?
         break;
 
       case true: // 로그인 상태일 때
         postGroupJoin(invitationKey)
           .then((res) => {
-            setisDone((prev) => true);
-            // 그룹 가입 완료 페이지로 이동
-            navigate(convertPath('/'));
+            setisDone((prev) => true); // 그룹 가입 완료 페이지로 이동
           })
           .catch((err) => {
             console.log(err);
-            // 승인 오류
           });
         break;
     }
@@ -47,7 +40,7 @@ const InvitedPage = ({}: Component): JSX.Element => {
     <InvitationDone />
   ) : (
     <div>
-      {isModal && <div>로그인 모달</div>}
+      {isModal && <LoginOrSignup redirectPage={'/invited/' + invitationKey} />}
       <ErrorBoundary fallback={<p>유효하지 않은 초대입니다</p>}>
         <Suspense fallback={<div>초대장 로딩</div>}>
           <Invitation invitationKey={invitationKey} />
