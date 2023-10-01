7d2df429bc7aa37d5235015a10d3a9c782379318
localgaji <oorongcha@naver.com>
2023-10-01 18:48:54 +0900

    fix: 로그인&가입 버튼 컴포넌트 import 수정, 노출조건 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import PageContainer from 'components/atoms/PageContainer';
-import LoginOrSignup from 'components/organisms/LoginOrSignup';
+import LoginOrSignup from 'components/molecules/LoginOrSignup';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'states/store';
@@ -12,7 +12,7 @@ const MainPage = ({}: Component): JSX.Element => {
   return (
     <PageContainer isHeader={islogin} isBottonBar={false}>
       <div className="w-full bg-slate-400 h-80 mb-8">임시 온보딩 컴포넌트</div>
-      <LoginOrSignup />
+      {islogin ? '로그인됨' : <LoginOrSignup />}
     </PageContainer>
   );
 };
