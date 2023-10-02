b592f1507ed18215b9d5f493dce23b563514ab33
localgaji <oorongcha@naver.com>
2023-10-02 20:03:54 +0900

    fix: 응답 처리 시 로그인 url 구분하는 과정 제거

--- src/apis/instance.tsx
+++ src/apis/instance.tsx
@@ -28,10 +28,6 @@ export const requestDefault = instance.interceptors.request.use(
 export const responseInterceptors = instance.interceptors.response.use(
   (response) => {
     console.log(response);
-    // 다른 데이터는 body에 담는데 토큰은 header에 담으므로 로그인 요청일땐 따로 지정
-    if (response.config.url?.includes('/auth')) {
-      return response.headers.authorization;
-    }
     return response;
   },
   (error) => {
