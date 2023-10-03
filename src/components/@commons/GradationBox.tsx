5043ca60776549f9d018507c4e77a13ebe2f5bee
localgaji <oorongcha@naver.com>
2023-10-04 05:03:41 +0900

    fix: gradationBox 수정

--- src/components/@commons/GradationBox.tsx
+++ src/components/@commons/GradationBox.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
-type Props = {
-  margin: number;
-  children: any;
-};
+// type Props = {
+//   margin: number;
+//   children: any;
+// };
 
-const GradationBox = ({ margin, children }: Props) => {
-  return <div className="rounded-lg bg-white p-5 shadow-md">{children}</div>;
-};
+// const GradationBoxNo = ({ margin, children }: Props) => {
+//   return <div className="rounded-lg bg-white p-5 shadow-md">{children}</div>;
+// };
+
+const GradationBox = styled.div<{ width?: string }>`
+  background: ${({ theme }) => theme.color.backgroundColor};
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.25);
+  width: ${(props) => (props.width ? props.width : '')};
+`;
 
 export default GradationBox;
