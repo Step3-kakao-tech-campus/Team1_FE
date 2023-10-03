5e76207c24f987c8be7e08971b0c31d1b9a152f9
localgaji <oorongcha@naver.com>
2023-10-03 22:02:14 +0900

    comment: 주석 처리 코드 제거

--- src/components/@commons/FlexContainer.tsx
+++ src/components/@commons/FlexContainer.tsx
@@ -2,45 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  children: any;
-  gap?: string;
-  padding?: string;
-  direction?: string;
-  wFull?: boolean;
-  hFull?: boolean;
-  justify?: string;
-  align?: string;
-  backgroundColor?: string;
-}
-
-// const FlexContainer = ({
-//   children,
-//   gap,
-//   padding,
-//   direction,
-//   wFull,
-//   hFull,
-//   justify,
-//   align,
-//   backgroundColor,
-// }: Props): JSX.Element => {
-//   return (
-//     <Container
-//       $gap={gap}
-//       $padding={padding}
-//       $direction={direction}
-//       $wFull={wFull}
-//       $hFull={hFull}
-//       $justify={justify}
-//       $align={align}
-//       $backgroundColor={backgroundColor}
-//     >
-//       {children}
-//     </Container>
-//   );
-// };
-
-const FlexContainer = styled.div<{
   $gap?: string;
   $padding?: string;
   $direction?: string;
@@ -49,7 +10,9 @@ const FlexContainer = styled.div<{
   $justify?: string;
   $align?: string;
   $backgroundColor?: string;
-}>`
+}
+
+const FlexContainer = styled.div<Props>`
   width: ${(props) => (props.$wFull ? '100%' : '')};
   height: ${(props) => (props.$hFull ? '100%' : '')};
 
@@ -66,15 +29,3 @@ const FlexContainer = styled.div<{
 `;
 
 export default FlexContainer;
-
-// const bgcolor = (props: any) => {
-//   const bgcolor: string = props.$backgroundColor;
-
-//   interface T {
-//     [index: string]: any;
-//   }
-//   const obj = props.theme.color as T;
-
-//   console.log(obj[bgcolor]);
-//   return obj[bgcolor] !== undefined ?  ;
-// };
