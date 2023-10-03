4b7cdb5dcb3186f4e55ad853dcea6facd56ee352
localgaji <oorongcha@naver.com>
2023-10-04 06:05:12 +0900

    commet: 주석 제거

--- src/components/@commons/GradationBox.tsx
+++ src/components/@commons/GradationBox.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// type Props = {
-//   margin: number;
-//   children: any;
-// };
-
-// const GradationBoxNo = ({ margin, children }: Props) => {
-//   return <div className="rounded-lg bg-white p-5 shadow-md">{children}</div>;
-// };
-
 const GradationBox = styled.div<{ width?: string }>`
   background: ${({ theme }) => theme.color.backgroundColor};
   box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.25);
