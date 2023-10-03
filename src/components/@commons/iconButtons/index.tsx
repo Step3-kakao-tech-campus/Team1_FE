0b212bacfba7645e3fdfe7ec9df24bbd08c003a5
localgaji <oorongcha@naver.com>
2023-10-04 06:08:26 +0900

    fix: 코드 오류 수정

--- src/components/@commons/iconButtons/index.tsx
+++ src/components/@commons/iconButtons/index.tsx
@@ -1,45 +1,47 @@
-import { styled } from 'styled-components';
 import React from 'react';
 
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { IoIosCloseCircleOutline } from 'react-icons/io';
 import { IoIosArrowForward } from 'react-icons/io';
 import { IoIosArrowBack } from 'react-icons/io';
+import { myTheme } from 'styles/myTheme';
 
-type Props = {
-  onClick: () => void;
-  size: string;
+interface Props {
+  onClick: any;
+  size?: string;
   color: string;
-};
+}
+const defaultSize: string = '24px';
+const defaultColor: string = myTheme.color.textColor;
 
 export const BackButton = ({ onClick, size, color }: Props) => {
   return (
-    <button onClick={() => onClick()}>
-      <IoMdArrowRoundBack size={size} color={color} />
+    <button onClick={onClick}>
+      <IoMdArrowRoundBack size={size ? size : defaultSize} color={color ? color : defaultColor} />
     </button>
   );
 };
 
 export const CloseButton = ({ onClick, size, color }: Props) => {
   return (
-    <div onClick={() => onClick()}>
-      <IoIosCloseCircleOutline size={size} color={color} />
-    </div>
+    <button onClick={onClick}>
+      <IoIosCloseCircleOutline size={size ? size : defaultSize} color={color ? color : defaultColor} />
+    </button>
   );
 };
 
 export const NextButton = ({ onClick, size, color }: Props) => {
   return (
-    <button onClick={() => onClick()}>
-      <IoIosArrowForward size={size} color={color} />
+    <button onClick={onClick}>
+      <IoIosArrowForward size={size ? size : defaultSize} color={color ? color : defaultColor} />
     </button>
   );
 };
 
 export const PrevButton = ({ onClick, size, color }: Props) => {
   return (
-    <button onClick={() => onClick()}>
-      <IoIosArrowBack size={size} color={color} />
+    <button onClick={onClick}>
+      <IoIosArrowBack size={size ? size : defaultSize} color={color ? color : defaultColor} />
     </button>
   );
 };
