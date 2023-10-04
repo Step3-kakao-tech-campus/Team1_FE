ba55e9cdaa913ca2a25ef87fcf5d3a5e3f9dbc07
localgaji <oorongcha@naver.com>
2023-10-05 04:14:20 +0900

    style: 필요없는 타입 제거

--- src/apis/getSch.ts
+++ src/apis/getSch.ts
@@ -1,7 +1,9 @@
 import instance from 'apis/instance';
 
+interface Return {}
+
 // 헤더: 토큰, 바디: 키
-export const getMonthly = async (month: number, year: number) => {
+export const getMonthly = async (year: number, month: number) => {
   const response = await instance.post(`/scheduler/home`, {
     month: month,
     year: year,
