e44f8308f47a5efd1f910efef8d9f98ac9ac1127
harin9 <83578728+harin9@users.noreply.github.com>
2023-10-04 13:10:03 +0900

    fix: ProfileImage 컴포넌트 props 수정

--- src/components/@commons/ProfileImage.tsx
+++ src/components/@commons/ProfileImage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { HiUserCircle } from 'react-icons/hi';
 
-type Props = {
+interface Props {
   size?: string;
   color?: string;
-};
+}
 
 const ProfileImage = ({ size, color }: Props) => {
   return <HiUserCircle size={size ? size : '30'} color={color ? color : '#828282'} />;
