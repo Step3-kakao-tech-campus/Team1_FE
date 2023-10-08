e081fe4f9b087ff9170406700147b5a2c6f8e731
localgaji <oorongcha@naver.com>
2023-10-08 16:48:43 +0900

    feat: 메인화면 private route 생성

---
+++ src/auth/HomePrivate.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from 'states/store';
+import AdminHomePage from 'components/admin-home/AdminHomePage';
+import NoGroupPage from 'components/admin-home/NoGroupPage';
+import OnBoardingPage from 'components/OnBoardingPage';
+
+const HomePrivate = () => {
+  const loginState = useSelector((state: RootState) => state.login);
+  const isLogin: boolean = loginState.islogin;
+  const isAdim: boolean = loginState.userData.isAdmin;
+  const isGroup: boolean = loginState.userData?.groupName.length > 0;
+
+  return (
+    <>
+      {isLogin ? (
+        isAdim ? (
+          isGroup ? (
+            <AdminHomePage />
+          ) : (
+            <NoGroupPage />
+          )
+        ) : isGroup ? (
+          <div>알바홈</div>
+        ) : (
+          <div>매니저에게 초대받으세요</div>
+        )
+      ) : (
+        <Outlet />
+      )}
+    </>
+  );
+};
+
+export default HomePrivate;
