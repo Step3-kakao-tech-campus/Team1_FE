24fd12585d76fb88141264117e48cf2a01196d7d
localgaji <oorongcha@naver.com>
2023-10-09 07:30:07 +0900

    chore: jotai 설치

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,33 @@ import InvitedPage from 'pages/InvitedPage';
 import AddGroupPage from 'pages/AddGroupPage';
 import HomePrivate from 'auth/HomePrivate';
 import LogoutOnlyPrivate from 'auth/LogoutOnlyPrivate';
+import OnBoardingPage from 'pages/OnBoardingPage';
+import { Provider } from 'jotai';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={myTheme}>
-      <BrowserRouter>
-        <ErrorBoundary fallback={<p>에러... app.tsx</p>}>
-          <Suspense fallback={<p>로딩... app.tsx</p>}>
-            <Routes>
-              <Route element={<HomePrivate />}>
-                <Route path={convertPath('/')} />
-              </Route>
+      <Provider>
+        <BrowserRouter>
+          <ErrorBoundary fallback={<p>에러... app.tsx</p>}>
+            <Suspense fallback={<p>로딩... app.tsx</p>}>
+              <Routes>
+                <Route element={<HomePrivate />}>
+                  <Route path={convertPath('/')} element={<OnBoardingPage />} />
+                </Route>
 
-              <Route element={<LogoutOnlyPrivate />}>
-                <Route path={convertPath('/signup')} element={<SignupPage />} />
-                <Route path={convertPath('/login/kakao')} element={<KakaoAuthPage />} />
-              </Route>
+                <Route element={<LogoutOnlyPrivate />}>
+                  <Route path={convertPath('/signup')} element={<SignupPage />} />
+                  <Route path={convertPath('/login/kakao')} element={<KakaoAuthPage />} />
+                </Route>
 
-              <Route path={convertPath('/invited/:invitationKey')} element={<InvitedPage />} />
-              <Route path={convertPath('/addGroup')} element={<AddGroupPage />} />
-            </Routes>
-          </Suspense>
-        </ErrorBoundary>
-      </BrowserRouter>
+                <Route path={convertPath('/invited/:invitationKey')} element={<InvitedPage />} />
+                <Route path={convertPath('/addGroup')} element={<AddGroupPage />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   );
 }
