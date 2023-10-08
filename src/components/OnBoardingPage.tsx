3f2fc1d4078d4418db59c1a7d6802ae6d8ee141a
localgaji <oorongcha@naver.com>
2023-10-08 16:49:51 +0900

    rename: page 레벨로 변경

---
+++ src/components/OnBoardingPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginOrSignup from 'components/@commons/LoginOrSignup';
+import FlexContainer from 'components/@commons/FlexContainer';
+import PageContainer from './@commons/PageContainer';
+
+const OnBoardingPage = (): JSX.Element => {
+  return (
+    <PageContainer withoutBottonBar withoutHeader>
+      <FlexContainer $wFull>
+        <div className="w-full bg-slate-400 h-80 mb-8">임시 온보딩 컴포넌트</div>
+        <LoginOrSignup />
+      </FlexContainer>
+    </PageContainer>
+  );
+};
+
+export default OnBoardingPage;
