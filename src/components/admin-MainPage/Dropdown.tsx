4e13f78fbd705295a5d4da31ae8466351d31c8c4
localgaji <oorongcha@naver.com>
2023-10-09 07:34:28 +0900

    feat: jotai 로 전역 상태관리

--- src/components/admin-MainPage/Dropdown.tsx
+++ src/components/admin-MainPage/Dropdown.tsx
@@ -1,52 +1,61 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import FlexContainer from 'components/@commons/FlexContainer';
+import Text from 'components/@commons/Text';
+import { memberAtom } from './AdminScheduleSection';
+import { useAtom } from 'jotai';
 
 interface Props<T> {
   members: T[];
-  selected: T | null;
-  setSelected: React.Dispatch<React.SetStateAction<T | null>>;
 }
 
 interface MemberType {
+  memberId: number;
   name: string;
 }
 
-const Dropdown = <T extends MemberType>({ members, selected, setSelected }: Props<T>): JSX.Element => {
+const Dropdown = <T extends MemberType>({ members }: Props<T>): JSX.Element => {
+  const [member, setMember] = useAtom(memberAtom);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOnClick = () => {
     setIsOpen((prev) => !prev);
   };
 
-  const itemOnClick = (member: T) => {
-    setSelected((prev) => member);
+  const itemOnClick = (m: T) => {
+    setMember((prev) => m);
     setIsOpen((prev) => false);
   };
 
   return (
-    <div>
-      <div className="flex justify-between items-center align-middle" onClick={handleOnClick}>
-        <span>{!!selected ? selected.name : '선택'}</span>
-        <span>{isOpen ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}</span>
-      </div>
+    <Container>
+      <FlexContainer $direction="row" onClick={handleOnClick} $align="center" $justify="space-between">
+        <Text>{member.name !== '' ? member.name : '선택'}</Text>
+        <Text>{isOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}</Text>
+      </FlexContainer>
       {isOpen && (
-        <Contents>
+        <FlexContainer $gap="0" $padding="8px 0">
           {members &&
             members.map((member: T) => (
               <ol onClick={() => itemOnClick(member)} key={member.name}>
-                {member.name}
+                <Text>{member.name}</Text>
               </ol>
             ))}
-        </Contents>
+        </FlexContainer>
       )}
-    </div>
+    </Container>
   );
 };
 
 export default Dropdown;
 
-const Contents = styled.div`
+const Container = styled.div`
+  width: 200px;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  padding: 2px 24px;
+  background-color: ${({ theme }) => theme.color.lightGray};
 `;
