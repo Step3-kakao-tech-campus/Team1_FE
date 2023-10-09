0e5654deb0e83296584ac620730466f7697225da
localgaji <oorongcha@naver.com>
2023-10-10 05:48:54 +0900

    type: 제네릭 사용

--- src/utils/signupValidator.ts
+++ src/utils/signupValidator.ts
@@ -1,4 +1,8 @@
-const signupValidator = (userInfo: any): boolean => {
+interface Info {
+  userName: string;
+  agreement: boolean;
+}
+const signupValidator = <T extends Info>(userInfo: T): boolean => {
   return userInfo.userName.length >= 2 && userInfo.agreement == true;
 };
 
