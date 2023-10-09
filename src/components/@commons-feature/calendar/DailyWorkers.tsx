c91f1061c10f08aba5957a1769189e1bdc3610b9
localgaji <oorongcha@naver.com>
2023-10-10 05:51:23 +0900

    refactor: styled-component 파일 분리

--- src/components/@commons-feature/calendar/DailyWorkers.tsx
+++ src/components/@commons-feature/calendar/DailyWorkers.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import FlexContainer from '../../@commons/FlexContainer';
+import FlexContainer from 'components/@commons/FlexContainer';
 import { useQuery } from '@tanstack/react-query';
 import { getDailyWorkers } from 'apis/getSchedule';
-import styled from 'styled-components';
-import Text from '../../@commons/Text';
+
+import Text from 'components/@commons/Text';
+import { NameBox, TitleBox } from './DailyWorkersStyle';
 
 interface Props {
   date: string;
+}
+
+interface TimeData {
+  title: string;
+  workerList: Worker[];
+  startTime: string;
+  endTime: string;
+}
+
+interface Worker {
+  name: string;
+  memberId: number;
 }
 
 const DailyWorkers = ({ date }: Props): JSX.Element => {
@@ -17,7 +30,7 @@ const DailyWorkers = ({ date }: Props): JSX.Element => {
     <FlexContainer $wFull>
       {obj && (
         <FlexContainer $direction="row">
-          {obj.data.schedule.map((timeData: any) => (
+          {obj.data.schedule.map((timeData: TimeData) => (
             <FlexContainer key={timeData.title} $wFull $gap="10px">
               <TitleBox $time={timeData.title}>
                 <Text block size="lg" weight="semiBold">
@@ -28,7 +41,7 @@ const DailyWorkers = ({ date }: Props): JSX.Element => {
                 </Text>
               </TitleBox>
               <FlexContainer $gap="8px">
-                {timeData.workerList.map((w: any) => (
+                {timeData.workerList.map((w: Worker) => (
                   <NameBox key={w.name}>
                     <Text>{w.name}</Text>
                   </NameBox>
@@ -43,22 +56,3 @@ const DailyWorkers = ({ date }: Props): JSX.Element => {
 };
 
 export default DailyWorkers;
-
-const TitleBox = styled(FlexContainer)<{ $time?: string }>`
-  width: 100%;
-  background-color: ${(props) =>
-    props.$time &&
-    (props.$time === '오픈'
-      ? props.theme.color.open
-      : props.$time === '미들'
-      ? props.theme.color.middle
-      : props.theme.color.close)};
-  align-items: center;
-  gap: 0;
-  padding: 4px;
-`;
-
-const NameBox = styled.div`
-  background-color: ${({ theme }) => theme.color.lightGray};
-  text-align: center;
-`;
