74312fecee98ffc7de8c003e9383f915825e911f
localgaji <oorongcha@naver.com>
2023-10-11 04:28:51 +0900

    refactor: 파라미터 객체로 전달

--- src/apis/groupInvite.ts
+++ src/apis/groupInvite.ts
@@ -1,17 +1,12 @@
 import instance from 'apis/instance';
 
 // 그룹초대 승인
-export const postGroupJoin = (key: string) => {
-  return instance.post(`/group/invitation`, {
-    invitationKey: key,
-  });
+export const postGroupJoin = (body: { key: string }) => {
+  return instance.post(`/group/invitation`, body);
 };
 
 // 매장명 확인, (추가: 이미 소속된 그룹인지 여부)
-export const getGroupInfo = (inviteKey: string) => {
-  const params = {
-    inviteKey: inviteKey,
-  };
+export const getGroupInfo = (params: { inviteKey: string }) => {
   return instance.get(`/group/invitation/information`, { params });
 };
 
