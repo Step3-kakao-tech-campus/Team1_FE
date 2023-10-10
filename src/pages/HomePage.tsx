f077aa3dfb167b2d8665f3651fc7d299982ec898
localgaji <oorongcha@naver.com>
2023-10-11 06:54:27 +0900

    rename: 이름 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,11 @@
 import React, { Suspense } from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from 'states/store';
 import AdminHomePage from 'pages/admin-MainPage/AdminHomePage';
 import AdminNoGroupPage from 'pages/admin-MainPage/AdminNoGroupPage';
 import OnBoardingPage from 'pages/OnBoardingPage';
 
-const HomePrivate = () => {
+const HomePage = () => {
   const loginState = useSelector((state: RootState) => state.login);
   const isLogin: boolean = loginState.islogin;
   const isAdmin: boolean = loginState.userData.isAdmin;
@@ -35,4 +34,4 @@ const HomePrivate = () => {
   );
 };
 
-export default HomePrivate;
+export default HomePage;
