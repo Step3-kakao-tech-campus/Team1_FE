c926f8237ccfbea6ee66caf53a11bbb04fa41905
localgaji <oorongcha@naver.com>
2023-10-11 06:40:32 +0900

    feat: 주차 선택 페이지 생성

---
+++ src/pages/admin-SelectWeekPage/SelectWeekPage.tsx
@@ -0,0 +1,13 @@
+import FlexContainer from 'components/@commons/FlexContainer';
+import PageContainer from 'components/@commons/PageContainer';
+import React from 'react';
+
+const SelectWeekPage = (): JSX.Element => {
+  return (
+    <PageContainer>
+      <FlexContainer></FlexContainer>
+    </PageContainer>
+  );
+};
+
+export default SelectWeekPage;
