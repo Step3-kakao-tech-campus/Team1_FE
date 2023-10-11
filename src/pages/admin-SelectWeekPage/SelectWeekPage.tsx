4cf1c872b3b35bf3dd2e6696ce4bb9432220b458
localgaji <oorongcha@naver.com>
2023-10-12 08:16:29 +0900

    feat: 주차 선택시 나타나는 디테일 컴포넌트 생성

--- src/pages/admin-SelectWeekPage/SelectWeekPage.tsx
+++ src/pages/admin-SelectWeekPage/SelectWeekPage.tsx
@@ -1,28 +1,26 @@
 import FlexContainer from 'components/@commons/FlexContainer';
 import PageContainer from 'components/@commons/PageContainer';
 import React, { Suspense } from 'react';
-import Text from 'components/@commons/Text';
-import { atom, useAtom } from 'jotai';
+import { atom } from 'jotai';
 import CalenderOutter from 'components/@commons-feature/calendar/CalenderOutter';
-import SelectWeekInner from './SelectWeekInner';
+import MonthlyContents from './MonthlyContents';
+import WeeklyDatail from './WeeklyDatail';
 
 export const weekStatusMonthAtom = atom({ year: new Date().getFullYear(), month: new Date().getMonth() });
-export const selectedWeekAtom = atom('');
+export const selectedWeekAtom = atom({ startWeekDate: '', weekStatus: '' });
 
 const SelectWeekPage = (): JSX.Element => {
-  const [selectedWeek] = useAtom(selectedWeekAtom);
   return (
     <PageContainer justify="start">
-      <FlexContainer $wFull $align="center">
-        {selectedWeek === '' && <Text>주차를 선택하세요</Text>}
-      </FlexContainer>
-
       <FlexContainer $wFull>
         <CalenderOutter monthDataAtom={weekStatusMonthAtom}>
           <Suspense>
-            <SelectWeekInner />
+            <MonthlyContents />
           </Suspense>
         </CalenderOutter>
+      </FlexContainer>
+      <FlexContainer $wFull $align="center">
+        <WeeklyDatail />
       </FlexContainer>
     </PageContainer>
   );
---
+++ src/pages/admin-SelectWeekPage/WeeklyDatail.tsx
@@ -0,0 +1,84 @@
+import { useAtom } from 'jotai';
+import React, { Suspense } from 'react';
+import { selectedWeekAtom } from './SelectWeekPage';
+import { useNavigate } from 'react-router-dom';
+import { convertPath } from 'apis/convertURI';
+import SubmitButton from 'components/@commons/SubmitButton';
+import Text from 'components/@commons/Text';
+import { useQuery } from '@tanstack/react-query';
+import useWeekSelector from 'hooks/useWeekSelector';
+import { getApplyStatus } from 'apis/getApplyStatus';
+import DailyWorkersTemplate from 'components/@commons-feature/calendar/DailyWorkersTemplate';
+import { dateToString } from 'utils/dateToString';
+import { getDailyWorkers } from 'apis/getSchedule';
+
+const WeeklyDatail = (): JSX.Element => {
+  const [selectedWeek] = useAtom(selectedWeekAtom);
+
+  switch (selectedWeek.weekStatus) {
+    case 'allocatable':
+      return <OpenDetail startWeekDate={selectedWeek.startWeekDate} />;
+    case 'inProgress':
+      return (
+        <Suspense>
+          <InProgressDetail startWeekDate={selectedWeek.startWeekDate} />
+        </Suspense>
+      );
+    case 'closed':
+      return (
+        <Suspense>
+          <ClosedDetail startWeekDate={selectedWeek.startWeekDate} />
+        </Suspense>
+      );
+  }
+  return <Text>주차를 선택해 주세요</Text>;
+};
+
+export default WeeklyDatail;
+
+const OpenDetail = ({ startWeekDate }: { startWeekDate: string }) => {
+  const navigate = useNavigate();
+  const openHandler = () => {
+    navigate(convertPath('/newSchedule/open'), { state: { startWeekDate: startWeekDate } });
+  };
+  return <SubmitButton onClick={openHandler}>스케줄 모집 시작하기</SubmitButton>;
+};
+
+const InProgressDetail = ({ startWeekDate }: { startWeekDate: string }) => {
+  const { data: applicantsStatusRes } = useQuery(
+    ['getApplyStatus', startWeekDate],
+    () => getApplyStatus({ startWeekDate: startWeekDate }),
+    { suspense: true },
+  );
+  const navigate = useNavigate();
+
+  const closeHandler = () => {
+    navigate(convertPath('/newSchedule/close'), { state: { startWeekDate: startWeekDate } });
+  };
+
+  const { day, WeekBarComponent } = useWeekSelector(0);
+  return (
+    <>
+      <SubmitButton onClick={closeHandler}>모집 마감하고 배정하기</SubmitButton>
+      <WeekBarComponent />
+      <DailyWorkersTemplate dailyData={applicantsStatusRes?.data.applyStatus[day]} />
+    </>
+  );
+};
+
+const ClosedDetail = ({ startWeekDate }: { startWeekDate: string }) => {
+  const { day, WeekBarComponent } = useWeekSelector(0);
+  const [y, m, d] = startWeekDate.split('-').map((e) => Number.parseInt(e));
+  const date = dateToString(new Date(y, m, d + day));
+
+  const { data: scheduleRes } = useQuery(['getDailyWorkers', day], () => getDailyWorkers({ date: date }), {
+    suspense: true,
+  });
+
+  return (
+    <>
+      <WeekBarComponent />
+      <DailyWorkersTemplate dailyData={scheduleRes?.data.schedule} />
+    </>
+  );
+};
