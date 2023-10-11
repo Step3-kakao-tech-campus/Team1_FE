dc16dbb949ed4dc0320d26a0c0a20e840d2e8da0
localgaji <oorongcha@naver.com>
2023-10-12 01:50:28 +0900

    feat: 모집하기 페이지에 캘린더 등록

--- src/pages/admin-SelectWeekPage/SelectWeekPage.tsx
+++ src/pages/admin-SelectWeekPage/SelectWeekPage.tsx
@@ -1,11 +1,29 @@
 import FlexContainer from 'components/@commons/FlexContainer';
 import PageContainer from 'components/@commons/PageContainer';
-import React from 'react';
+import React, { Suspense } from 'react';
+import Text from 'components/@commons/Text';
+import { atom, useAtom } from 'jotai';
+import CalenderOutter from 'components/@commons-feature/calendar/CalenderOutter';
+import SelectWeekInner from './SelectWeekInner';
+
+export const weekStatusMonthAtom = atom({ year: new Date().getFullYear(), month: new Date().getMonth() });
+export const selectedWeekAtom = atom('');
 
 const SelectWeekPage = (): JSX.Element => {
+  const [selectedWeek] = useAtom(selectedWeekAtom);
   return (
-    <PageContainer>
-      <FlexContainer></FlexContainer>
+    <PageContainer justify="start">
+      <FlexContainer $wFull $align="center">
+        {selectedWeek === '' && <Text>주차를 선택하세요</Text>}
+      </FlexContainer>
+
+      <FlexContainer $wFull>
+        <CalenderOutter monthDataAtom={weekStatusMonthAtom}>
+          <Suspense>
+            <SelectWeekInner />
+          </Suspense>
+        </CalenderOutter>
+      </FlexContainer>
     </PageContainer>
   );
 };
