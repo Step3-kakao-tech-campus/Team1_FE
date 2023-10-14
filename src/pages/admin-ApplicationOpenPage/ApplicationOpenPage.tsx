d10199a5c58d14e745711f1072e15fd49b85fcda
localgaji <oorongcha@naver.com>
2023-10-14 14:07:00 +0900

    feat: 스케줄 모집시작 전역상태 선언

--- src/pages/admin-ApplicationOpenPage/ApplicationOpenPage.tsx
+++ src/pages/admin-ApplicationOpenPage/ApplicationOpenPage.tsx
@@ -1,7 +1,23 @@
+import PageContainer from 'components/@commons/PageContainer';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { atom, useAtomValue } from 'jotai';
+
+interface TimeData {
+  title: string;
+  startTime: string;
+  endTime: string;
+}
+
+export const timeTemplateAtom = atom<TimeData[]>([]);
+export const weeklyPeopleAtom = atom<number[][]>([[], [], [], [], [], [], []]);
+export const openStepAtom = atom(1);
 
 const ApplicationOpenPage = (): JSX.Element => {
-  return <div></div>;
+  const startWeekDate = useLocation().state.startWeekDate;
+  const step = useAtomValue(openStepAtom);
+
+  return <PageContainer justify="start">content</PageContainer>;
 };
 
 export default ApplicationOpenPage;
