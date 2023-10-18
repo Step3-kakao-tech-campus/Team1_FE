3d6b95d8c10da3093b0ea7867e6d44b49eb6ac50
localgaji <oorongcha@naver.com>
2023-10-19 03:46:22 +0900

    type: 외부 선언 type 재사용

--- src/components/DailyWorkers/DailyWorkersTemplate.tsx
+++ src/components/DailyWorkers/DailyWorkersTemplate.tsx
@@ -3,23 +3,12 @@ import FlexContainer from 'components/@commons/FlexContainer';
 
 import Text from 'components/@commons/Text';
 import { NameBox, TitleBox } from './DailyWorkersStyle';
+import { DailyWorkTimeData, WorkerData } from 'apis/getSchedule';
 
-interface TimeData {
-  title: string;
-  workerList: Worker[];
-  startTime: string;
-  endTime: string;
-}
-
-interface Worker {
-  name: string;
-  memberId: number;
-}
-
-const DailyWorkersTemplate = ({ dailyData }: { dailyData: TimeData[] }): JSX.Element => {
+const DailyWorkersTemplate = ({ dailyData }: { dailyData: DailyWorkTimeData[] | undefined }): JSX.Element => {
   return (
     <FlexContainer $direction="row" $wFull $gap="3%">
-      {dailyData.map((timeData: TimeData) => (
+      {dailyData?.map((timeData: DailyWorkTimeData) => (
         <FlexContainer key={timeData.title} $wFull $gap="10px">
           <TitleBox $time={timeData.title}>
             <Text block size="lg" weight="semiBold">
@@ -30,7 +19,7 @@ const DailyWorkersTemplate = ({ dailyData }: { dailyData: TimeData[] }): JSX.Ele
             </Text>
           </TitleBox>
           <FlexContainer $gap="8px">
-            {timeData.workerList.map((w: Worker, i) => (
+            {timeData.workerList.map((w: WorkerData, i) => (
               <NameBox key={w.name + i}>
                 <Text>{w.name}</Text>
               </NameBox>
