9bba5fe04ea2133c62c9a8a3c4a5ff0424073fda
localgaji <oorongcha@naver.com>
2023-10-20 05:06:41 +0900

    feat: 알바 신청주 선택 페이지 생성

---
+++ src/pages/alba/WeeklyStatusPage/index.tsx
@@ -0,0 +1,8 @@
+import SelectWeekPage from 'pages/SelectWeekPage';
+import React from 'react';
+
+const AlbaSelectWeekPage = (): JSX.Element => {
+  return <SelectWeekPage isAdmin={false} />;
+};
+
+export default AlbaSelectWeekPage;
