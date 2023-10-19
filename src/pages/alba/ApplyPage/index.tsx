1535eeb176b1d662ed536b23ef9f8477a49b8e08
localgaji <oorongcha@naver.com>
2023-10-20 04:26:19 +0900

    feat: 알바 신청 페이지 생성

---
+++ src/pages/alba/ApplyPage/index.tsx
@@ -0,0 +1,9 @@
+import PageContainer from 'components/@commons/PageContainer';
+import SelectWeekPage from 'pages/admin/SelectWeekPage';
+import React from 'react';
+
+const ApplyPage = (): JSX.Element => {
+  return <SelectWeekPage />;
+};
+
+export default ApplyPage;
