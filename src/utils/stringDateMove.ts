e40ab88cf01d414525eb6d451cc4def672098ac7
localgaji <oorongcha@naver.com>
2023-10-22 06:51:01 +0900

    feat: 한국어 날짜 표시 추가

--- src/utils/stringDateMove.ts
+++ src/utils/stringDateMove.ts
@@ -5,3 +5,9 @@ export const stringDateMove = (stringDate: string, dx: number) => {
   const dateObj = new Date(y, m - 1, d + dx);
   return dateToString(dateObj);
 };
+
+export const stringDateMoveKor = (string: string, dx: number) => {
+  const [y, m, d] = string.split('-').map((e) => Number.parseInt(e));
+  const date = new Date(y, m - 1, d + dx);
+  return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+};
