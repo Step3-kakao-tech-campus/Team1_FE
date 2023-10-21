531cc073b2040b90c3f2b902545eeac7d05109d0
localgaji <oorongcha@naver.com>
2023-10-21 19:17:01 +0900

    feat: 날짜, 타임 데이터 가공

--- src/pages/alba/ApplyPage/PreviewSection/index.tsx
+++ src/pages/alba/ApplyPage/PreviewSection/index.tsx
@@ -3,41 +3,54 @@ import BorderBox from 'components/@commons/BorderBox';
 import FlexContainer from 'components/@commons/FlexContainer';
 import Text from 'components/@commons/Text';
 import useApply from 'hooks/alba/useApply';
-import { useAtomValue, useSetAtom } from 'jotai';
 import { stringDateMove } from 'utils/stringDateMove';
 import weekdayArray from 'utils/weekdayArray';
-import { applyStepAtom, weeklySelectAtom } from '..';
 import SubmitButton from 'components/@commons/SubmitButton';
 import { useNavigate } from 'react-router-dom';
 import { convertPath } from 'apis/convertURI';
+import { myTheme } from 'styles/myTheme';
 
 const PreviewSection = ({ startWeekDate }: { startWeekDate: string }): JSX.Element => {
-  const { findTimeData } = useApply(startWeekDate);
+  const { weeklySelect, findTimeData, setStep } = useApply(startWeekDate);
+  const navigate = useNavigate();
 
-  const weeklySelect = useAtomValue(weeklySelectAtom);
-  const setStep = useSetAtom(applyStepAtom);
-  const prevBtnHandler = () => {
-    setStep(1);
+  const processTime = (dayIndex: number) => {
+    const processed = weeklySelect[dayIndex]
+      .filter((object) => object.isChecked)
+      .map((object) => findTimeData(object.workTimeId).title);
+
+    if (processed.length === 0) return '휴무';
+
+    return processed.join(' | ');
   };
 
-  const navigate = useNavigate();
+  const stringDateToKor = (string: string, dayIndex: number) => {
+    const [y, m, d] = string.split('-').map((e) => Number.parseInt(e));
+    const date = new Date(y, m - 1, d + dayIndex);
+    return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+  };
+
   return (
     <FlexContainer $wFull $gap="36px">
-      <button onClick={prevBtnHandler}> 편집하기 </button>
+      <button onClick={() => setStep(1)}> 편집하기 </button>
       <FlexContainer $wFull>
         {weekdayArray.map((weekday, dayIndex) => (
-          <BorderBox gradation key={stringDateMove(startWeekDate, dayIndex)}>
-            <FlexContainer $wFull $direction="row" $padding="20px">
-              <Text margin="0">{stringDateMove(startWeekDate, dayIndex)}</Text>
-              <Text margin="0 auto 0 0">{weekday.kor}</Text>
-              {weeklySelect[dayIndex].map(
-                (object) =>
-                  object.isChecked && (
-                    <Text margin="0 0 0 auto" key={object.workTimeId}>
-                      {findTimeData(object.workTimeId).title}
-                    </Text>
-                  ),
-              )}
+          <BorderBox gradation key={`${dayIndex}요일`}>
+            <FlexContainer $wFull $direction="row" $padding="20px 24px" $gap="0">
+              <Text size="lg" margin="0">
+                {stringDateToKor(startWeekDate, dayIndex)}
+              </Text>
+              <Text size="lg" margin="0 auto 0 28px">
+                {weekday.kor}
+              </Text>
+              <Text
+                margin="0 0 0 auto"
+                size="lg"
+                color={processTime(dayIndex) === '휴무' ? myTheme.color.gray : '#0066FF'}
+                weight="semiBold"
+              >
+                {processTime(dayIndex)}
+              </Text>
             </FlexContainer>
           </BorderBox>
         ))}
