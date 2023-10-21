030ec1526728235f7d0fc178a93e504fbbf0652d
localgaji <oorongcha@naver.com>
2023-10-22 05:11:47 +0900

    remove: 사용하지 않는 atom 삭제

--- src/pages/alba/ApplyPage/index.tsx
+++ src/pages/alba/ApplyPage/index.tsx
@@ -5,10 +5,9 @@ import { atom, useAtomValue } from 'jotai';
 import weekdayArray from 'utils/weekdayArray';
 import TimeSelectSection from './TimeSelectSection';
 import PreviewSection from './PreviewSection';
-import { SelectedTimeData, TimeData } from 'apis/types';
+import { SelectedTimeData } from 'apis/types';
 
 export const weeklySelectAtom = atom<SelectedTimeData[][]>(weekdayArray.map(() => []));
-export const timeTemplateAtom = atom<{ [index: number]: TimeData }>({});
 export const applyStepAtom = atom(1);
 
 const ApplyPage = (): JSX.Element => {
