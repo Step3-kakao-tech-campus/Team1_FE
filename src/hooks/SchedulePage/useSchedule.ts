00ffac99d1c968bad3ac87f1575bc7fd1fff4c94
localgaji <oorongcha@naver.com>
2023-10-24 01:25:27 +0900

    style: 선언 위치 수정

--- src/hooks/SchedulePage/useSchedule.ts
+++ src/hooks/SchedulePage/useSchedule.ts
@@ -1,20 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMonthly } from 'apis/schedule/getMonthly';
-import { useAtom, useAtomValue } from 'jotai';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import React from 'react';
 import useLogin from 'hooks/useLogin';
 import { dateAtom, memberAtom, monthAtom, workTimeAtom } from 'pages/SchedulePage/states';
 
 const useSchedule = () => {
-  const [selectedDate, setSelectedDate] = useAtom(dateAtom);
   const nowMember = useAtomValue(memberAtom);
   const { year, month } = { ...useAtomValue(monthAtom) };
-  const isAdmin = useLogin().getLoginState().isAdmin;
 
-  const [, setWorkTime] = useAtom(workTimeAtom);
+  const setWorkTime = useSetAtom(workTimeAtom);
   const { data: scheduleData } = useQuery(
     ['getMonthly', year, month, nowMember.memberId],
-    () => getMonthly({ year: year, month: month, memberId: nowMember.memberId, isAdmin: isAdmin }),
+    () =>
+      getMonthly({
+        year: year,
+        month: month,
+        memberId: nowMember.memberId,
+        isAdmin: useLogin().getLoginState().isAdmin,
+      }),
     {
       suspense: true,
       enabled: nowMember.isSelected === true,
@@ -24,6 +28,7 @@ const useSchedule = () => {
     },
   );
 
+  const [selectedDate, setSelectedDate] = useAtom(dateAtom);
   const dateOnClick = (isFixed: boolean, date: string) => {
     const newObj = { date: date, isFixed: isFixed };
     setSelectedDate((prev) => newObj);
