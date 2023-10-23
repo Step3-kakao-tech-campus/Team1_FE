d051fc7cc767046f2efdc9297a1057c45bcbca50
localgaji <oorongcha@naver.com>
2023-10-24 01:28:21 +0900

    feat: 그룹생성 페이지 입력값 validator 적용

--- src/pages/admin/AddGroupPage/index.tsx
+++ src/pages/admin/AddGroupPage/index.tsx
@@ -2,13 +2,17 @@ import PageContainer from 'components/@commons/PageContainer';
 import DoneSection from 'pages/admin/AddGroupPage/DoneSection';
 import React, { useState } from 'react';
 import FormSection from './FormSection';
+import { useQuery } from '@tanstack/react-query';
+import { getMyInfo } from 'apis/userInfo';
 
 const AddGroupPage = (): JSX.Element => {
   // 해당 매니저가 이미 그룹을 가지고 있는 경우 : 리다이렉트 "/"
+
+  // const { data: myInfo } = useQuery(['myInfo'], getMyInfo);
   const [isDone, setIsDone] = useState(false);
 
   return (
-    <PageContainer withoutHeader withoutBottonBar padding="0 40px">
+    <PageContainer withoutHeader withoutBottonBar padding="0 40px" gap="52px">
       {!isDone && <FormSection doneStateHandler={() => setIsDone((prev) => true)} />}
       {isDone && <DoneSection />}
     </PageContainer>
