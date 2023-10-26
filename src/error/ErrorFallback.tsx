2b427bb044dac75821fce1088e5e4fd3d890e06a
localgaji <oorongcha@naver.com>
2023-10-27 07:35:09 +0900

    feat: 에러 처리 추가

--- src/error/ErrorFallback.tsx
+++ src/error/ErrorFallback.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { convertPath } from 'apis/convertURI';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import ErrorPage from './ErrorPage';
 import { ErrorData } from 'apis/types';
 import { AdminNoGroupPage, AdminNoMemberPage } from 'pages/admin/ETCMainPage';
@@ -35,38 +35,35 @@ const ErrorFallback = ({ error, resetErrorBoundary }: Props) => {
   const code = error.response?.data?.code as number;
 
   switch (code) {
-    case -21000:
-      // 토큰 유효하지 않음
-      resetErrorBoundary();
-      alert('로그인이 만료되었습니다. 다시 로그인 해주세요.');
-      removeLoginData();
-      navigate(convertPath('/'));
-      return <></>;
-
     case -10000:
       // 타임 아웃
       return <ErrorPage message="서버 에러" btnHandler={() => resetErrorBoundary()} btnText="다시 시도하기" />;
 
     case -20001:
       // 이미 그룹이 있는데 초대 접속
-      return <></>;
+      alert('이미 소속된 그룹이 있습니다');
+      return <Navigate to={convertPath('/')} />;
+
+    case -20002:
+      // 이미 모집 시작됨
+      alert('해당 주차 모집이 이미 시작되었습니다');
+      return <Navigate to={convertPath('/newSchedule')} />;
 
     case -20003:
       // 이미 모집 마감
-      return (
-        <ErrorPage
-          message="이미 해당 주차 모집이 마감되었습니다"
-          btnHandler={() => {
-            resetErrorBoundary();
-            navigate(convertPath('/'));
-          }}
-          btnText="메인으로"
-        />
-      );
+      alert('해당 주차 모집이 이미 마감되었습니다');
+      return <Navigate to={convertPath('/newSchedule')} />;
 
     case -20004:
       // 유효하지 않은 초대
       return <ErrorPage message="유효하지 않은 초대입니다" goMain />;
+
+    case -21000:
+      // 토큰 유효하지 않음
+      resetErrorBoundary();
+      removeLoginData();
+      alert('로그인이 만료되었습니다. 다시 로그인 해주세요.');
+      return <Navigate to={convertPath('/')} />;
 
     case -21001:
       // 그룹 없음 : no group page
