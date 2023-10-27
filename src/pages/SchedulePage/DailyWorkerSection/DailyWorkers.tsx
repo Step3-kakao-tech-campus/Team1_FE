44521538ae14dfd508628ab2d549494b875cee8e
localgaji <oorongcha@naver.com>
2023-10-28 03:31:42 +0900

    refactor: 상태관리 추가

--- src/pages/SchedulePage/DailyWorkerSection/DailyWorkers.tsx
+++ src/pages/SchedulePage/DailyWorkerSection/DailyWorkers.tsx
@@ -1,15 +1,40 @@
 import { useQuery } from '@tanstack/react-query';
 
 import DailyWorkersTemplate from 'components/DailyWorkers/DailyWorkersTemplate';
-import FlexContainer from 'components/@commons/FlexContainer';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getDailyWorkers } from 'apis/schedule/getDailyWorkers';
+import { useAtomValue } from 'jotai';
+import { dateAtom } from '../states';
+import NotFixedDateBox from 'components/DailyWorkers/NotFixedDateBox';
+import SubmitButton from 'components/@commons/SubmitButton';
+import { getLoginData } from 'utils/loginDatahandlers';
 
-const DailyWorkers = ({ date }: { date: string }): JSX.Element => {
-  const { data: scheduleResponse } = useQuery([date], () => getDailyWorkers({ selectedDate: date }), {
-    suspense: true,
-  });
-  return <>{scheduleResponse && <DailyWorkersTemplate dailyData={scheduleResponse?.data.schedule} />}</>;
+const DailyWorkers = (): JSX.Element => {
+  const isAdmin = getLoginData().isAdmin;
+
+  const selectedDate = useAtomValue(dateAtom);
+  const { data: scheduleResponse } = useQuery(
+    [selectedDate],
+    () => getDailyWorkers({ selectedDate: selectedDate.date }),
+    {
+      suspense: true,
+    },
+  );
+
+  if (selectedDate.date === '') {
+    return <></>;
+  }
+
+  if (!selectedDate.isFixed) {
+    return <NotFixedDateBox />;
+  }
+
+  return (
+    <>
+      <DailyWorkersTemplate dailyData={scheduleResponse?.data.schedule} />
+      {!isAdmin && <SubmitButton>대타를 구하고 싶어요</SubmitButton>}
+    </>
+  );
 };
 
 export default DailyWorkers;
