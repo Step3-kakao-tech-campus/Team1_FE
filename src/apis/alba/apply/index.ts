aedb66e1417ddc2c0fa9661a1ba9aa8a4593e5c9
localgaji <oorongcha@naver.com>
2023-10-30 22:44:04 +0900

    refactor: 데이터 처리 추가

--- src/apis/alba/apply/index.ts
+++ src/apis/alba/apply/index.ts
@@ -1,25 +1,39 @@
 import instance from 'apis/instance';
-import { SelectedTimeData, TimeData } from 'apis/types';
+import { SelectedTimeData, TimeData, TimeWithIdData } from 'apis/types';
+import { AxiosResponse } from 'axios';
 import { strTimeProcessor } from 'utils/strTimeProcessor';
 
 export const getApplyForm = async (params: GetParams): Promise<GetReturn> => {
-  const response = await instance.get(`/schedule/application`, { params });
-  const selected = response.data.selected;
+  const response: AxiosResponse<GetResponse> = await instance.get(`/schedule/application`, { params });
 
   const templates: { [index: number]: TimeData } = {};
   for (let timeObj of response.data.template) {
-    templates[timeObj.workTimeId] = {
+    templates[timeObj.worktimeId] = {
       title: timeObj.title,
       startTime: strTimeProcessor(timeObj.startTime),
       endTime: strTimeProcessor(timeObj.endTime),
     };
   }
 
+  const selected = response.data.selected.map((dailyArray) => {
+    return dailyArray.map((workersObj) => {
+      return { ...templates[workersObj.worktimeId], isChecked: workersObj.isChecked };
+    });
+  });
+
   return { selected, templates };
 };
 
 interface GetParams {
   startWeekDate: string;
+}
+
+interface GetResponse {
+  template: TimeWithIdData[];
+  selected: {
+    worktimeId: number;
+    isChecked: boolean;
+  }[][];
 }
 
 interface GetReturn {
