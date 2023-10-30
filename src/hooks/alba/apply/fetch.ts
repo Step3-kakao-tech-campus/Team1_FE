6314e86ee30557aec1e76d1847a4b0037eb9ebe7
localgaji <oorongcha@naver.com>
2023-10-30 22:45:19 +0900

    refactor: 커스텀훅 관심사 분리

---
+++ src/hooks/alba/apply/fetch.ts
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getApplyForm, putApply } from 'apis/alba/apply';
+import { weeklySelectAtom } from 'pages/alba/ApplyPage';
+
+export const useGetApplyForm = (startWeekDate: string) => {
+  const setWeeklySelect = useSetAtom(weeklySelectAtom);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const { data } = useQuery(
+    ['getApplyForm', startWeekDate],
+    () =>
+      getApplyForm({
+        startWeekDate: startWeekDate,
+      }),
+    {
+      onSuccess: (data) => {
+        if (data === undefined) return;
+        setWeeklySelect(data.selected);
+        setIsLoading(false);
+      },
+    },
+  );
+
+  return { data, isLoading };
+};
+
+export const usePutApplyForm = (startWeekDate: string, onSuccess: () => void) => {
+  const weeklySelect = useAtomValue(weeklySelectAtom);
+
+  const { mutate } = useMutation(() => putApply({ weekStartDate: startWeekDate, apply: weeklySelect }), {
+    onSuccess: onSuccess,
+  });
+
+  return { mutate };
+};
--- src/pages/alba/ApplyPage/PreviewSection/index.tsx
+++ src/pages/alba/ApplyPage/PreviewSection/index.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import BorderBox from 'components/@commons/BorderBox';
 import FlexContainer from 'components/@commons/FlexContainer';
 import Text from 'components/@commons/Text';
-import useApply from 'hooks/alba/useApply';
+import usePreviewSelected from 'hooks/alba/apply/usePreviewSelected';
 import weekdayArray from 'utils/weekdayArray';
 import SubmitButton from 'components/@commons/SubmitButton';
-import { Link } from 'react-router-dom';
-import { convertPath } from 'apis/convertURI';
 import { myTheme } from 'styles/myTheme';
 import { stringDateMoveKor } from 'utils/dateToString';
 
 const PreviewSection = ({ startWeekDate }: { startWeekDate: string }): JSX.Element => {
-  const { setStep, worktimeIdProcessor } = useApply(startWeekDate);
+  const { submitApplyHandler, checkedTimeOnly, goSelectHandler } = usePreviewSelected(startWeekDate);
 
   return (
     <FlexContainer $wFull $gap="36px">
-      <button onClick={() => setStep('checkTime')}> 편집하기 </button>
+      <button onClick={goSelectHandler}> 편집하기 </button>
       <FlexContainer $wFull>
         {weekdayArray.map((weekday, dayIndex) => (
           <BorderBox gradation key={`${dayIndex}요일`}>
@@ -29,19 +27,17 @@ const PreviewSection = ({ startWeekDate }: { startWeekDate: string }): JSX.Eleme
               <Text
                 margin="0 0 0 auto"
                 size="lg"
-                color={worktimeIdProcessor(dayIndex) === '휴무' ? myTheme.color.gray : '#0066FF'}
+                color={checkedTimeOnly(dayIndex) === '휴무' ? myTheme.color.gray : '#0066FF'}
                 weight="semiBold"
               >
-                {worktimeIdProcessor(dayIndex)}
+                {checkedTimeOnly(dayIndex)}
               </Text>
             </FlexContainer>
           </BorderBox>
         ))}
       </FlexContainer>
 
-      <Link to={convertPath('/')}>
-        <SubmitButton>제출하기</SubmitButton>
-      </Link>
+      <SubmitButton onClick={submitApplyHandler}>제출하기</SubmitButton>
     </FlexContainer>
   );
 };
