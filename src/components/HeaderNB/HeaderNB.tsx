f1db38fc09e8c34114837a1b3b026eee768d3728
localgaji <oorongcha@naver.com>
2023-10-31 04:40:35 +0900

    feat: 대체 텍스트 추가

--- src/components/HeaderNB/HeaderNB.tsx
+++ src/components/HeaderNB/HeaderNB.tsx
@@ -4,7 +4,6 @@ import {
   HeaderInnerBox,
   HeaderButton,
   HeaderLeftMenuGroup,
-  HeaderRightMenuGroup,
   HeaderTitleCont,
 } from 'components/HeaderNB/HeaderNBStyels';
 import Sidebar from 'components/Sidebar';
@@ -13,42 +12,25 @@ import { useLocation } from 'react-router-dom';
 import { Hamburger } from 'components/@commons/icons';
 import { getLoginData } from 'utils/loginDatahandlers';
 
+const isAdmin = getLoginData().isAdmin;
+
 const HeaderNB = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
 
   const nowPath = useLocation().pathname;
-  const isAdmin = getLoginData().isAdmin;
-
-  const albaTitle: { [index: string]: string } = {
-    '/': '내 스케줄',
-    '/apply': '신청하기',
-    '/apply/selectTimes': '신청하기',
-  };
-
-  const adminTitle: { [index: string]: string } = {
-    '/': '확정 스케줄',
-    '/newSchedule': '모집하기',
-    '/newSchedule/open': '모집 시작하기',
-    '/newSchedule/close': '모집 마감하기',
-  };
 
   return (
     <>
       <HeaderContainer>
         <HeaderInnerBox>
           <HeaderLeftMenuGroup>
-            <HeaderButton onClick={() => setIsOpen(true)}>
+            <HeaderButton onClick={() => setIsOpen(true)} aria-label="메뉴">
               <Hamburger />
             </HeaderButton>
           </HeaderLeftMenuGroup>
           <HeaderTitleCont>
             <Text size="lg">{isAdmin ? adminTitle[nowPath] : albaTitle[nowPath]}</Text>
           </HeaderTitleCont>
-          {/* <HeaderRightMenuGroup>
-            <HeaderButton>
-              <Alarm />
-            </HeaderButton>
-          </HeaderRightMenuGroup> */}
         </HeaderInnerBox>
       </HeaderContainer>
 
@@ -58,3 +40,16 @@ const HeaderNB = (): JSX.Element => {
 };
 
 export default HeaderNB;
+
+const albaTitle: { [index: string]: string } = {
+  '/': '내 스케줄',
+  '/apply': '신청하기',
+  '/apply/selectTimes': '신청하기',
+};
+
+const adminTitle: { [index: string]: string } = {
+  '/': '확정 스케줄',
+  '/newSchedule': '모집하기',
+  '/newSchedule/open': '모집 시작하기',
+  '/newSchedule/close': '모집 마감하기',
+};
