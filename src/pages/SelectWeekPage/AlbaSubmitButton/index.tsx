d008e6ff3562469a0022e7da022a8f77095655e3
localgaji <oorongcha@naver.com>
2023-10-30 22:54:13 +0900

    refactor: 커스텀 훅 적용

--- src/pages/SelectWeekPage/AlbaSubmitButton/index.tsx
+++ src/pages/SelectWeekPage/AlbaSubmitButton/index.tsx
@@ -1,20 +1,17 @@
-import { useNavigate } from 'react-router';
-import { convertPath } from 'apis/convertURI';
 import SubmitButton from 'components/@commons/SubmitButton';
 import Text from 'components/@commons/Text';
 import React from 'react';
 import { useAtomValue } from 'jotai';
-import { selectedWeekAtom } from 'pages/SelectWeekPage';
+import { selectedWeekAtom } from 'pages/SelectWeekPage/states';
+import useWeeklyDetail from 'hooks/SelectWeekPage/useWeeklyDetail';
 
 const AlbaSubmitButton = (): JSX.Element => {
-  const navigate = useNavigate();
   const selectedWeek = useAtomValue(selectedWeekAtom);
-  const submitHandler = () => {
-    navigate(convertPath('/apply/selectTimes'), { state: { startWeekDate: selectedWeek.startWeekDate } });
-  };
+  const { albaBtnHandler } = useWeeklyDetail();
+
   return (
     <>
-      {selectedWeek.startWeekDate !== '' && <SubmitButton onClick={submitHandler}>신청하러가기</SubmitButton>}
+      {selectedWeek.startWeekDate !== '' && <SubmitButton onClick={albaBtnHandler}>신청하러가기</SubmitButton>}
       {selectedWeek.startWeekDate === '' && <Text> 주차를 선택해 주세요</Text>}
     </>
   );
