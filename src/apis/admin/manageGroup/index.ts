859e64e12952d776731731fd0ac82fac7e2eb049
localgaji <oorongcha@naver.com>
2023-10-30 22:48:58 +0900

    type: 타입 선언 이동

--- src/apis/admin/manageGroup/index.ts
+++ src/apis/admin/manageGroup/index.ts
@@ -1,16 +1,10 @@
 import instance from 'apis/instance';
+import { AddNeweGroupForm } from 'apis/types';
 import { AxiosResponse } from 'axios';
 
-export const postAddNewGroup = (body: postRequest) => {
+export const postAddNewGroup = (body: AddNeweGroupForm) => {
   return instance.post(`/group`, body);
 };
-
-export interface postRequest {
-  marketName: string;
-  marketNumber: string;
-  mainAddress: string;
-  detailAddress: string;
-}
 
 // 초대링크 발급
 export const getInviteKey = (): Promise<AxiosResponse<GetResponse>> => {
