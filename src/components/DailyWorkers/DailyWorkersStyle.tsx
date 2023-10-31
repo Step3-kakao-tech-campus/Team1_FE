bb3434751c0dbaa55c80528e8b130e83ec819d63
localgaji <oorongcha@naver.com>
2023-10-31 19:45:48 +0900

    refactor: props 수정

--- src/components/DailyWorkers/DailyWorkersStyle.tsx
+++ src/components/DailyWorkers/DailyWorkersStyle.tsx
@@ -2,9 +2,9 @@ import FlexContainer from 'components/@commons/FlexContainer';
 import styled from 'styled-components';
 import { timeColors } from 'utils/colors';
 
-export const TitleBox = styled(FlexContainer)<{ $time: string; $timeIndex: number }>`
+export const TitleBox = styled(FlexContainer)<{ $timeIndex: number }>`
   width: 100%;
-  background-color: ${(props) => props.$time && timeColors(props.$timeIndex)};
+  background-color: ${(props) => props.$timeIndex && timeColors(props.$timeIndex)};
   align-items: center;
   gap: 0;
   padding: 4px;
