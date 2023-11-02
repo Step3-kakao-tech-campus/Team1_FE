dfaf0528a34b2655e1986011fea27c609089c70a
localgaji <oorongcha@naver.com>
2023-11-02 20:25:41 +0900

    feat: refetchOnWindowFocus 옵션 해제

--- src/hooks/useGetMyInfo.ts
+++ src/hooks/useGetMyInfo.ts
@@ -4,10 +4,15 @@ import { getLoginData } from 'utils/loginDatahandlers';
 
 const useGetMyInfo = () => {
   const isLogin = getLoginData().isLogin;
-  const { data: myInfo } = useQuery(['myInfo'], getMyInfo, { suspense: true, cacheTime: 0, enabled: isLogin });
+  const { data: myInfo } = useQuery(['myInfo'], getMyInfo, {
+    suspense: true,
+    cacheTime: 0,
+    enabled: isLogin,
+    refetchOnWindowFocus: false,
+  });
 
   const userName = myInfo?.data.userName;
-  const groupName = myInfo?.data.groupName;
+  const groupName = myInfo?.data.groupName || null;
   const members = myInfo?.data.members || [];
 
   return {
