999c868aefe0ece7bcb9c74ee9abd2e8aae21026
localgaji <oorongcha@naver.com>
2023-11-03 19:58:11 +0900

    feat: 주차 선택 페이지 접속 시 open data 초기화

--- src/hooks/SelectWeekPage/useSelectWeek.ts
+++ src/hooks/SelectWeekPage/useSelectWeek.ts
@@ -1,8 +1,9 @@
-import { useAtomValue, useSetAtom } from 'jotai';
-import React from 'react';
 import { WeekStatusData, WeekStatusTypes } from 'apis/types';
-import { getLoginData } from 'utils/loginDatahandlers';
+import useTimeTemplate from 'hooks/admin/ApplicationOpenPage/useTimeTemplate';
+import { useAtomValue, useSetAtom } from 'jotai';
 import { selectedWeekAtom, weekStatusMonthAtom } from 'pages/SelectWeekPage/states';
+import React from 'react';
+import { getLoginData } from 'utils/loginDatahandlers';
 
 const useSelectWeek = () => {
   const isAdmin = getLoginData().isAdmin;
@@ -28,9 +29,16 @@ const useSelectWeek = () => {
   const nowMonth = useAtomValue(weekStatusMonthAtom);
   const { year, month } = nowMonth;
 
+  // 선택 주 초기화
   React.useEffect(() => {
     setSelectedWeek({ startWeekDate: '', weekStatus: '' });
   }, [year, month]);
+
+  // 모집 상태 초기화
+  const { initializeOpenData } = useTimeTemplate();
+  React.useEffect(() => {
+    initializeOpenData();
+  }, []);
 
   return { weekOnClickHandler, statusConverter };
 };
