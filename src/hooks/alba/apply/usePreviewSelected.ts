74465d58c793be5e2838c3dfeeb1fe95e3631a49
localgaji <oorongcha@naver.com>
2023-11-04 23:32:05 +0900

    comment: 주석 추가

--- src/hooks/alba/apply/usePreviewSelected.ts
+++ src/hooks/alba/apply/usePreviewSelected.ts
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useAtomValue } from 'jotai';
-import { weeklySelectAtom } from 'pages/alba/ApplyPage/states';
-import { usePutApplyForm } from './fetch';
-import { useNavigate } from 'react-router-dom';
 import { convertPath } from 'apis/convertURI';
 import usePopUpPage from 'hooks/usePopUpPage';
+import { useAtomValue } from 'jotai';
+import { weeklySelectAtom } from 'pages/alba/ApplyPage/states';
+import { useNavigate } from 'react-router-dom';
+import { usePutApplyForm } from './fetch';
 
 const usePreviewSelected = (startWeekDate: string) => {
   const weeklySelect = useAtomValue(weeklySelectAtom);
@@ -17,17 +16,18 @@ const usePreviewSelected = (startWeekDate: string) => {
     return processed.join(' | ');
   };
 
+  // 수정하러 가기
+  const goSelectHandler = () => {
+    popUpOffHandler();
+  };
+
+  // 제출 요청
   const navigate = useNavigate();
   const postOnSuccess = () => {
     navigate(convertPath('/'));
     popUpOffHandler();
   };
   const { mutate } = usePutApplyForm(startWeekDate, postOnSuccess);
-
-  const goSelectHandler = () => {
-    popUpOffHandler();
-  };
-
   const submitApplyHandler = () => {
     mutate();
   };
