b49663bda3a24f25de2b7bcc4d272c65646eb4c3
localgaji <oorongcha@naver.com>
2023-11-07 19:37:39 +0900

    test: 테스트용 모킹 파일

---
+++ tests/mock/responseBody/getSchedule.ts
@@ -0,0 +1,142 @@
+export const getMonthly = {
+  schedule: [
+    {
+      date: '2023-10-30',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-10-31',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-01',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-02',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-03',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-04',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-05',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-06',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-07',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-08',
+      workTime: ['미들'],
+    },
+    {
+      date: '2023-11-09',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-10',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-11',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-12',
+      workTime: ['미들'],
+    },
+    {
+      date: '2023-11-13',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-14',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-15',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-16',
+      workTime: ['미들'],
+    },
+    {
+      date: '2023-11-17',
+      workTime: ['오픈', '미들'],
+    },
+    {
+      date: '2023-11-18',
+      workTime: [],
+    },
+    {
+      date: '2023-11-19',
+      workTime: ['오픈', '미들'],
+    },
+  ],
+  work_summary: {
+    weekly: 15.5,
+    monthly: 61.5,
+  },
+};
+
+export const getDailyWorker = {
+  schedule: [
+    {
+      title: '오픈',
+      startTime: '10:00:00',
+      endTime: '12:00:00',
+      workerList: [
+        {
+          userId: 1,
+          name: '라이언',
+        },
+        {
+          userId: 2,
+          name: '어피치',
+        },
+      ],
+    },
+    {
+      title: '미들',
+      startTime: '12:00:00',
+      endTime: '16:00:00',
+      workerList: [
+        {
+          userId: 3,
+          name: '무지',
+        },
+        {
+          userId: 2,
+          name: '어피치',
+        },
+      ],
+    },
+    {
+      title: '마감',
+      startTime: '16:00:00',
+      endTime: '20:00:00',
+      workerList: [
+        {
+          userId: 3,
+          name: '무지',
+        },
+        {
+          userId: 4,
+          name: '춘식이',
+        },
+      ],
+    },
+  ],
+};
