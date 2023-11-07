1a02376d7d786f7ad923ed6a26c5b61a44b6d4c9
localgaji <oorongcha@naver.com>
2023-11-07 19:41:36 +0900

    refactor: testId 추가

--- src/components/DailyWorkersTable/index.tsx
+++ src/components/DailyWorkersTable/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import FlexContainer from 'components/@commons/FlexContainer';
-import Text from 'components/@commons/Text';
-import { NameBox, TitleBox } from './styles';
 import { TimeWorkerListData, UserData } from 'apis/types';
-import { strTimeProcessor } from 'utils/strTimeProcessor';
+import FlexContainer from 'components/@commons/FlexContainer';
 import GrayBox from 'components/@commons/GrayBox';
+import Text from 'components/@commons/Text';
+import { strTimeProcessor } from 'utils/strTimeProcessor';
+import { NameBox, TitleBox } from './styles';
 
 export const DailyWorkersTable = ({ dailyData }: { dailyData: TimeWorkerListData[] | undefined }): JSX.Element => {
   return (
-    <FlexContainer $direction="row" $wFull $gap="3%">
+    <FlexContainer $direction="row" $wFull $gap="3%" data-testId="일간근무표">
       {dailyData?.map((timeData: TimeWorkerListData, timeindex) => (
         <FlexContainer key={`${timeData.title}${timeindex}`} $wFull $gap="10px">
           <TitleBox $timeIndex={timeindex}>
@@ -34,7 +33,7 @@ export const DailyWorkersTable = ({ dailyData }: { dailyData: TimeWorkerListData
 
 export const NotFixedDateBox = (): JSX.Element => {
   return (
-    <GrayBox>
+    <GrayBox data-testId="미확정일간근무표">
       <Text size="lg" weight="medium">
         아직 확정된 스케줄이 없습니다
       </Text>
--- src/pages/SchedulePage/CalendarSection/CalenderConents.tsx
+++ src/pages/SchedulePage/CalendarSection/CalenderConents.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { DailyWorkTimeData } from 'apis/types';
 import { MonthBox, WeekGrid } from 'components/Calendar/CalendarStyle';
-import CalendarDayBox from './CalendarDayBox';
-import useSchedule from 'hooks/SchedulePage/useSchedule';
 import { useGetMonthly } from 'hooks/SchedulePage/fetch';
+import useSchedule from 'hooks/SchedulePage/useSchedule';
 import { useAtomValue } from 'jotai';
 import { dateAtom } from '../states';
+import CalendarDayBox from './CalendarDayBox';
 
 const CalenderConents = (): JSX.Element => {
   const { scheduleData } = useGetMonthly();
@@ -13,7 +12,7 @@ const CalenderConents = (): JSX.Element => {
   const selectedDate = useAtomValue(dateAtom);
 
   return (
-    <MonthBox $wFull>
+    <MonthBox $wFull data-testid="월간스케줄">
       {scheduleData?.table.map((weekArray: DailyWorkTimeData[], i) => (
         <WeekGrid key={`${i}주`} $aspectRatio="5.6">
           {weekArray.map((e: DailyWorkTimeData) => (
