a8bfc70d3bf4b9d9f7c50a005245f9e95b973d8f
localgaji <oorongcha@naver.com>
2023-11-09 18:59:21 +0900

    refactor: atom write 추가

--- src/pages/SelectWeekPage/states.ts
+++ src/pages/SelectWeekPage/states.ts
@@ -1,4 +1,11 @@
 import { atom } from 'jotai';
+import { SelectedMonthData } from 'pages/SchedulePage/states';
 
-export const weekStatusMonthAtom = atom({ year: new Date().getFullYear(), month: new Date().getMonth() });
-export const selectedWeekAtom = atom({ startWeekDate: '', weekStatus: '' });
+const weekStatusInitial = { year: new Date().getFullYear(), month: new Date().getMonth() };
+const selectedWeekInitial = { startWeekDate: '', weekStatus: '' };
+
+export const selectedWeekAtom = atom(selectedWeekInitial);
+export const weekStatusMonthAtom = atom(weekStatusInitial, (get, set, update: SelectedMonthData) => {
+  set(weekStatusMonthAtom, update);
+  set(selectedWeekAtom, selectedWeekInitial);
+});
