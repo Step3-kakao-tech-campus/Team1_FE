93313d95a846614f4f419b38708898c3f057955d
localgaji <oorongcha@naver.com>
2023-11-11 01:46:03 +0900

    refactor: 에러 코드 문자열 적용

--- src/error/defaultErrorHandler.ts
+++ src/error/defaultErrorHandler.ts
@@ -1,13 +1,14 @@
 import { convertPath } from 'apis/convertURI';
 import { ErrorData } from 'apis/types';
+import { stringErrorCode } from 'error/errorCode';
 import { loginDatahandlers } from 'utils/loginDatahandlers';
+
+const redirect = (url: string) => {
+  window.location.href = url;
+};
 
 export const defaultErrorHandler = (error: ErrorData) => {
   console.log('defaultErrorHandler', error);
-
-  const redirect = (url: string) => {
-    window.location.href = url;
-  };
 
   if (error.response === undefined) {
     alert('서버 오류');
@@ -16,66 +17,66 @@ export const defaultErrorHandler = (error: ErrorData) => {
   }
 
   // 서버 에러 응답
-  const code = error.response.data?.error.errorCode;
-  console.log('defaultErrorHandler', error.response.data?.error);
-  switch (code) {
-    case undefined:
-      alert(`에러코드가 없습니다`);
+  const code = error.response?.data?.error?.errorCode || 0;
+  const errorType = stringErrorCode(code);
+
+  switch (errorType) {
+    case 'GO_TO_JOIN':
       return;
 
-    case -10000:
-      // 타임 아웃
-      alert(`일시적인 네트워크 오류가 발생했습니다. 다시 시도하세요`);
+    case 'INVALID_INVITATION':
       return;
 
-    case -10001:
-      // 알 수 없는 오류
-      alert(`서버 오류`);
-      return;
-
-    case -10004:
-      // 폼 validation
-      alert(`입력값이 올바르지 않습니다`);
-      return;
-
-    case -10007:
-      // 카카오 oauth api 오류
-      alert(`서버 오류`);
-      return;
-
-    case -20000:
+    case 'ALEADY_USER':
       // 이미 가입되었는데 가입 요청
       alert('이미 가입되었습니다');
       redirect(convertPath('/'));
       return;
 
-    case -20001:
+    case 'ALEADY_HAS_GROUP':
       // 이미 그룹이 있음
       alert('이미 소속된 그룹이 있습니다');
       redirect(convertPath('/'));
       return;
 
-    case -20002:
+    case 'ALEADY_SCHEDULE_STARTED':
       // 이미 모집 시작되었는데 모집 시작
       alert('해당 주차 모집이 이미 시작되었습니다');
       redirect(convertPath('/newSchedule'));
       return;
 
-    case -20003:
+    case 'ALEADY_SCHEDULE_FIXED':
       // 이미 모집 마감되었는데 모집 마감
       alert('해당 주차 모집이 이미 마감되었습니다');
       redirect(convertPath('/newSchedule'));
       return;
 
-    case -21000:
+    case 'FORM_VALIDATION':
+      // 폼 validation
+      alert(`입력값이 올바르지 않습니다`);
+      return;
+
+    case 'TIMEOUT':
+      // 타임 아웃
+      alert(`일시적인 네트워크 오류가 발생했습니다. 다시 시도하세요`);
+      return;
+
+    case 'AUTHENTICATION':
       // 토큰 유효하지 않음
       alert('로그인이 만료되었습니다.');
       loginDatahandlers.removeLoginData();
       redirect(convertPath('/'));
       return;
 
-    default:
+    case 'AUTHORIZATION':
+      // 접근 권한
       alert(`잘못된 접근입니다`);
+      loginDatahandlers.removeLoginData();
+      redirect(convertPath('/'));
+      return;
+
+    default:
+      alert(`서버 오류`);
       loginDatahandlers.removeLoginData();
       redirect(convertPath('/'));
       return;
