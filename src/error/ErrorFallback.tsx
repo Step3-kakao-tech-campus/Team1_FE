d7053837980bdf60e28137feb014c3bafc9c00c5
localgaji <oorongcha@naver.com>
2023-11-11 23:07:15 +0900

    feat: 타임아웃 처리

--- src/error/ErrorFallback.tsx
+++ src/error/ErrorFallback.tsx
@@ -1,5 +1,6 @@
 import { convertPath } from 'apis/convertURI';
 import { ErrorFallbackProps } from 'apis/types';
+import { stringErrorCode } from 'error/errorCode';
 import { useNavigate } from 'react-router-dom';
 import ErrorPage from './ErrorPage';
 
@@ -8,6 +9,20 @@ const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
 
   const navigate = useNavigate();
 
+  const code = error.response?.data?.error?.errorCode || 0;
+  const errorType = stringErrorCode(code);
+
+  if (errorType === 'TIMEOUT')
+    return (
+      <ErrorPage
+        message="time out"
+        btnHandler={() => {
+          resetErrorBoundary();
+        }}
+        isNeedLogout={false}
+        btnText="다시 시도하기"
+      />
+    );
   return (
     <ErrorPage
       message="Something Went Wrong"
