fda475ff22fdd78e8f140dc53dff1b99eded0ddc
localgaji <oorongcha@naver.com>
2023-11-14 01:22:58 +0900

    fix: 주차 선택 초기화

--- src/pages/SelectWeekPage/hooks/useSelectWeek.ts
+++ src/pages/SelectWeekPage/hooks/useSelectWeek.ts
@@ -1,6 +1,7 @@
 import { WeekStatusData } from 'apis/types';
 import { useSetAtom } from 'jotai';
 import { selectedWeekAtom } from 'pages/SelectWeekPage/states';
+import { useEffect } from 'react';
 import { loginDatahandlers } from 'utils/loginDatahandlers';
 
 const useSelectWeek = () => {
@@ -12,6 +13,10 @@ const useSelectWeek = () => {
     const newObj = { startWeekDate: weekObj.dates[0], weekStatus: weekObj.weekStatus };
     setSelectedWeek(newObj);
   };
+
+  useEffect(() => {
+    setSelectedWeek({ startWeekDate: '', weekStatus: '' });
+  }, []);
 
   return { weekOnClickHandler };
 };
--- src/pages/admin/ApplicationClosePage/hooks/useClose.ts
+++ src/pages/admin/ApplicationClosePage/hooks/useClose.ts
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { convertPath } from 'apis/convertURI';
-import { postRecommends } from 'apis/admin/application/close';
 import { useMutation } from '@tanstack/react-query';
+import { postRecommends } from 'apis/admin/application/close';
+import { convertPath } from 'apis/convertURI';
+import { useSetAtom } from 'jotai';
+import { selectedWeekAtom } from 'pages/SelectWeekPage/states';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const useClose = (startWeekDate: string) => {
   // 선택된 후보, 후보 선택시 상태 업데이트
   const [candidate, setCandidate] = useState(0);
-
+  const setSelectedWeek = useSetAtom(selectedWeekAtom);
   // 제출 클릭시 post 요청
   const navigate = useNavigate();
   const { mutate } = useMutation(
     ['postRecommends'],
     () => postRecommends({ weekStartDate: startWeekDate, selection: candidate + 1 }),
     {
-      onSuccess: () => navigate(convertPath('/')),
+      onSuccess: () => {
+        navigate(convertPath('/'));
+        setSelectedWeek({ startWeekDate: '', weekStatus: '' });
+      },
     },
   );
   const submitHandler = () => {
