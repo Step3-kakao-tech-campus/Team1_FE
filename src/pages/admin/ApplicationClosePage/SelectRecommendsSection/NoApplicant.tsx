e0c2567c8be08a0aaba52a8158627bd78b0e278e
localgaji <oorongcha@naver.com>
2023-11-17 03:52:59 +0900

    fix: 신청자 미달일 때

--- src/pages/admin/ApplicationClosePage/SelectRecommendsSection/NoApplicant.tsx
+++ src/pages/admin/ApplicationClosePage/SelectRecommendsSection/NoApplicant.tsx
@@ -9,7 +9,7 @@ const NoApplicant = (): JSX.Element => {
     <FlexContainer $wFull $hFull>
       <FlexContainer $gap="36px">
         <FlexContainer $gap="8px">
-          <Text>아직 신청자가 없어 </Text>
+          <Text>신청자가 부족해</Text>
           <Text>해당 주차 스케줄을 마감할 수 없습니다.</Text>
         </FlexContainer>
         <Link to={convertPath('/newSchedule')}>
